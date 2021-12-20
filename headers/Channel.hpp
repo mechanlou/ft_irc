c116a02e2e368db5b44e8c621451c14214edc08d
Romain Kowalski <rkowalsk@z3r9p5.42lyon.fr>
2021-12-20 16:02:14 +0100

    modified channel

--- headers/Channel.hpp
+++ headers/Channel.hpp
@@ -8,7 +8,9 @@ class	Channel
 {
 	public :
 
-	// Channel(void);
+	std::string	name;
+
+	Channel(void);
 	Channel(std::string name, Client *creator);
 	Channel(Channel const &src);
 	virtual	~Channel(void);
@@ -17,14 +19,14 @@ class	Channel
 	void		add_regular_user(Client *user);
 	void		add_operator_user(Client *user);
 	void		remove_user(Client *user);
-	std::string	get_name(void) const;
-	void		set_name(std::string const &new_name);
+
+	std::vector<Client *>	get_chan_users(void) const;
+	std::vector<Client *>	get_chan_operators(void) const;
 
 	// int		msg_to_channel(const char *msg) const;
 
 	private :
 
-	std::string				_name;
 	std::vector<Client *>	_operators;
 	std::vector<Client *>	_all_users;
 };
