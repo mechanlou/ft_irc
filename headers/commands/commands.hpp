ae90622b554d2b65e22b444b80d4b7faa47e203d
William Peru <wperu@z4r8p5.42lyon.fr>
2022-01-26 17:39:16 +0100

    commande NICK waiting test

--- headers/commands/commands.hpp
+++ headers/commands/commands.hpp
@@ -6,12 +6,13 @@
 /*   By: wperu <wperu@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 16:11:39 by wperu             #+#    #+#             */
-/*   Updated: 2022/01/21 10:45:41 by wperu            ###   ########lyon.fr   */
+/*   Updated: 2022/01/26 17:23:42 by wperu            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Channel.hpp"
 #include "ircserver.hpp"
+#include "Client.hpp"
 
 class commands
 {
--- srcs/commands/client/NICK.cpp
+++ srcs/commands/client/NICK.cpp
@@ -6,7 +6,7 @@
 /*   By: wperu <wperu@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 11:07:43 by wperu             #+#    #+#             */
-/*   Updated: 2022/01/26 15:31:16 by wperu            ###   ########lyon.fr   */
+/*   Updated: 2022/01/26 17:37:25 by wperu            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ bool nick:: _checknick(std::string user, std::vector<Client *> *clients)
     for (std::vector<Client *>::iterator it = clients->begin(); it != clients->end(); it++)
 	{
 		Client *c = *it;
-		if (c->get_nick() == user)
+		if (c->get_nickname() == user)
 			return false;
 	}
 	return true;
@@ -45,6 +45,18 @@ bool nick::_validnick(std::string nick)
     return true;
 }
 
+bool nick::_is_informed(Client *dest, std::vector<Client *> informed)
+{
+	for(std::vector<Client *>::iterator it = informed.begin(); it != informed.end(); it++)
+	{
+		Client *search = *it;
+		if(search->get_nickname() == dest->get_nickname())
+			return true;
+	}
+	return false;
+}
+
+
 void nick:: _announcement_new_nick(std::string message, Client *cli,std::vector<Channel *> *chan)
 {
     std::vector<Client *> informe;
@@ -52,7 +64,17 @@ void nick:: _announcement_new_nick(std::string message, Client *cli,std::vector<
 	for(std::vector<Channel *>::iterator it = chan->begin(); it != chan->end(); it++)
 	{
 		Channel *c = *it;
-		if(c->is_members(cli->get_nick))
+		if(c->is_members(cli->get_nickname()))
+		{
+			std::vector<Client *> clis = c->get_members();
+			for(std::vector<Client *>::iterator its = clis.begin(); its != clis.end();its++)
+			{
+				Client *dest = *its;
+				if(!_is_informed(dest,informe))
+				send(dest->get_sock_fd(),message.c_str(),message.length(),0);
+				informe.push_back(dest);
+			}
+		}
 	}
 }
 
@@ -78,18 +100,18 @@ void nick::excute(std::string buf, Client *cli, std::vector<Channel *> *chan, st
     if (!_validnick(nick))
 	{
 		message = ":server " + std::string(ERR_ERRONEUSNICKNAME) + " nick: Erroneus nickname\r\n";
-		send(cli->getSd(), message.c_str(), message.length(), 0);
+		send(cli->get_sock_fd(), message.c_str(), message.length(), 0);
 		return ;
 	}
 	if (!_checknick(nick, Clients))
 	{
 		message = ":server " + std::string(ERR_NICKNAMEINUSE) + " nick: Nickname is already in use\r\n";
-		send(cli->getSd(), message.c_str(), message.length(), 0);
+		send(cli->get_sock_fd(), message.c_str(), message.length(), 0);
 		return ;
 	}
 
-    message = ":" + cli->getNick() + " NICK " + nick +"\r\n";
-	cli->setNick(nick);
+    message = ":" + cli->get_nickname() + " NICK " + nick +"\r\n";
+	cli->set_nickname(nick);
     
     _announcement_new_nick(message, cli, chan);
 }      
