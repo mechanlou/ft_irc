d9e75490be93eae0235d32f88fc5f2848b9d69ad
BadWolf <romainkowalski@hotmail.fr>
2022-02-07 19:19:47 +0100

    added notice

---
+++ headers/commands/NOTICE.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "ircserver.hpp"
+
+class notice : public commands
+{
+    private:
+		void	_send_channel(std::string msg, Client *src,
+			std::vector<Channel> *all_chans, std::vector<Client> *all_clients,
+			std::vector<pollfd> &fds, std::vector<std::string> args);
+		void	_send_client(std::string msg, Client *src,
+			std::vector<Channel> *all_chans, std::vector<Client> *all_clients,
+			std::vector<pollfd> &fds, std::vector<std::string> args);
+
+    public:
+        void	execute(std::string msg, Client *src,
+			std::vector<Channel> *all_chans, std::vector<Client> *all_clients,
+			std::vector<pollfd> &fds);
+        notice();
+        ~notice();
+};
---
+++ srcs/commands/NOTICE.cpp
@@ -0,0 +1,65 @@
+#include "NOTICE.hpp"
+
+notice::notice(void) {}
+
+notice::~notice(void) {}
+
+void	notice::_send_channel(std::string msg, Client *src,
+	std::vector<Channel> *all_chans, std::vector<Client> *all_clients,
+	std::vector<pollfd> &fds, std::vector<std::string> args)
+{
+	std::vector<Channel>::iterator	it_chan;
+	std::vector<Channel>::iterator	it_chan_end;
+
+	it_chan = all_chans->begin();
+	it_chan_end = all_chans->end();
+	while (it_chan != it_chan_end)
+	{
+		if (it_chan->get_name() == args[0])
+		{
+			if (it_chan->is_members(src->get_nickname()))
+				it_chan->msg_to_channel(msg.c_str(), fds);
+			break;
+		}
+		it_chan++;
+	}
+}
+
+void	notice::_send_client(std::string msg, Client *src,
+	std::vector<Channel> *all_chans, std::vector<Client> *all_clients,
+	std::vector<pollfd> &fds, std::vector<std::string> args)
+{
+	std::vector<Client>::iterator	it_cli;
+	std::vector<Client>::iterator	it_cli_end;
+
+	it_cli = all_clients->begin();
+	it_cli_end = all_clients->end();
+	while (it_cli != it_cli_end)
+	{
+		if (it_cli->get_nickname() == args[0])
+		{
+			send_msg_client(*it_cli, fds, msg.c_str());
+			break;
+		}
+		it_cli++;
+	}
+}
+
+void	notice::execute(std::string msg, Client *src, std::vector<Channel> *all_chans,
+	std::vector<Client> *all_clients, std::vector<pollfd> &fds)
+{
+	std::string					cmd;
+	std::vector<std::string>	args;
+
+	pars_msg(msg, cmd, args);
+	if (msg[0] != ':')
+		msg.insert(0, src->get_full_prefix());
+	if (args.size() == 0)
+		return ;
+	if (args.size() == 1)
+		return ;
+	if (args[0][0] == '#')
+		_send_channel(msg, src, all_chans, all_clients, fds, args);
+	else
+		_send_client(msg, src, all_chans, all_clients, fds, args);
+}
