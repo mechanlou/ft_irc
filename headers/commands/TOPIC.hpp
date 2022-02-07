5868a78039a45f82da97d708bd49a16286f2889d
wperu <wperu62@gmail.com>
2022-02-07 11:56:20 +0100

    topic update

--- headers/commands/TOPIC.hpp
+++ headers/commands/TOPIC.hpp
@@ -6,9 +6,20 @@
 /*   By: wperu <wperu@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 17:03:46 by wperu             #+#    #+#             */
-/*   Updated: 2022/02/03 17:04:07 by wperu            ###   ########lyon.fr   */
+/*   Updated: 2022/02/07 11:20:26 by wperu            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once 
 #include "commands.hpp"
 
+class topic : public commands
+{
+    private :
+        void get_cmd(std::string buf);
+
+    public:
+        topic();
+        ~topic();
+        void excute(std::string buf, client *cli, std::vecor<Channel *> *chan, std::vector<pollfd> &fds);
+}
--- srcs/commands/TOPIC.cpp
+++ srcs/commands/TOPIC.cpp
@@ -6,8 +6,83 @@
 /*   By: wperu <wperu@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 16:00:25 by wperu             #+#    #+#             */
-/*   Updated: 2022/02/03 16:00:39 by wperu            ###   ########lyon.fr   */
+/*   Updated: 2022/02/07 11:55:51 by wperu            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "TOPIC.hpp"
+
+topic::topic()
+{   
+}
+
+topic::~topic()
+{   
+}
+
+void topic::excute(std::string buf, Client *cli, std::vector<Channel *> *chan,std::vector<pollfd> &fds)
+{
+    Channel *tmp_chan;
+    get_cmd(buf);
+
+    if(_cmd.size() < 2)
+    {
+       err_needmoreparams(*cli, fds,buf.substr(0,5));
+       return; 
+    }
+    tmp_chan = _check_chan(_cmd[1], chan);
+    if(tmp_chan != NULL)
+    {
+        if(!tmp_chan->is_members(cli))
+        {
+            err_notochannel(*cli,fds,tmp_chan);
+            return;
+        }
+        else if (_cmd.size() == 2)
+        {
+            if(tmp_chan->get_topic().empty())
+            {
+                rpl_notopic(*cli, fds, _cmd[1]));
+                return;
+            }
+            else
+            {
+                rpl_topic(*cli, fds, _cmd[1], tmp_chan->get_topic());
+                return;
+            }
+        }
+        else
+        {
+            if(_cmd[2].front() == ':')
+                cur_chan->set_topic(&_cmd[2][1]);
+            else
+                cur_chan->set_topic(_cmd(2));
+        }
+    }
+    
+}
+
+void topic::get_cmd()
+{
+    char space = ' ';
+    int i = 0;
+    std::vector<std::string> cmd_tmp;
+    std::string line;
+    std::stringstream ss(buf);
+
+    while(stddd:getline(ss,line,space))
+    {
+        cmd_tmp.push_back(line);
+        i++;
+        if(i == 2)
+            space = '\n';
+    }
+    
+    while(cmd_tmp.back().back() == '\n' || cmd_tmp.back() == '\r')
+        cmd_tmp.back().pop_back();
+
+    _cmd.clear();
+    if(cmd_tmp[1] == ":")
+        cmd_tmp.pop_back();
+    _cmd = cmd_tmp;
+}
