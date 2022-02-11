2c6e21ca6a6095eb67c28bd7433000c99b89f539
Romain Kowalski <rkowalsk@z4r8p3.42lyon.fr>
2022-02-11 16:39:59 +0100

    fixed QUIT

--- srcs/commands/QUIT.cpp
+++ srcs/commands/QUIT.cpp
@@ -20,41 +20,11 @@ quit::~quit()
 {
 }
 
-bool quit::_is_informed(Client *dest, std::vector<Client *> informed)
-{
-
-    for (std::vector<Client *>::iterator it = informed.begin(); it != informed.end(); it++)
-	{
-		Client *search = *it;
-		if (search->get_nickname() == dest->get_nickname())
-			return true;
-	}
-	return false;
-}
-
-void quit::_announcement(std::string message, Client *cli, std::vector<Channel> *chan,std::vector<pollfd> &fds)
-{
-	std::vector<Client *> informed;
-	informed.push_back(cli);
-
-	for (std::vector<Channel >::iterator it = chan->begin(); it != chan->end(); it++)
-	{
-		if (it->is_members(cli->get_nickname()))
-		{
-			std::vector<Client *> cls = it->get_all_users();
-			it->remove_user(cli);
-			it->msg_to_channel(message.c_str(), fds);
-		}
-	}
-}
-
 int	quit::excute(std::string buf, Client *cli, std::vector<Channel> *chan, std::vector<Client> *clients,std::vector<pollfd> &fds)
 {
-    std::string bye = buf.substr(5, buf.length() - 5);
-	if (bye[0] == ':')
-		bye = bye.substr(1, bye.length() - 1);
+	std::string	announcement;
 
-	buf = ":" + cli->get_nickname() + "!" + cli->get_name() + "@" + cli->get_ip() + " QUIT :Quit: " + bye + END_OF_MSG;
-	_announcement(buf, cli, chan, fds);
+	announcement = ":" + cli->get_nickname() + "!" + cli->get_name() + "@" + cli->get_ip() + ' ' + buf;
+	broadcast_msg(*clients, fds, announcement.c_str());
 	return (close_connection(cli->get_sock_fd(), fds, *clients, *chan));
 }
